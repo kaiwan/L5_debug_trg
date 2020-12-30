b8dc6a0c8b04cf9fb41fe0ac7aad4f34fcc33f72
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-12-30 12:59:14 +0530

    wip..

--- kernel_debug/k_oops_warn_panic/oops2/oops2.c
+++ kernel_debug/k_oops_warn_panic/oops2/oops2.c
@@ -5,13 +5,12 @@
  *
  * Kaiwan NB, kaiwanTECH
  */
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/slab.h>
 #include "convenient.h"
 
-MODULE_LICENSE("Dual BSD/GPL");
+MODULE_LICENSE("Dual MIT/GPL");
 
 /*
  * On another tangent:
@@ -22,25 +21,23 @@ LEVEL1_DCACHE_LINESIZE             64
 LEVEL2_CACHE_LINESIZE              64
 LEVEL3_CACHE_LINESIZE              64
 LEVEL4_CACHE_LINESIZE              0
-$ 
- */ 
+$
+ */
 struct faker {
-  long longs[7];  // (on x86_64 a 'long' takes 8 bytes, so) use 8*7=56 bytes
-  char dumb[7];   // use 56+7=63 bytes; now 1 byte left in this cacheline!
-  long bad_cache_align; // use 63+8=71 bytes, thus spilling over the cacheline! Very naughty.
+	long longs[7];		// (on x86_64 a 'long' takes 8 bytes, so) use 8*7=56 bytes
+	char dumb[7];		// use 56+7=63 bytes; now 1 byte left in this cacheline!
+	long bad_cache_align;	// use 63+8=71 bytes, thus spilling over the cacheline! Very naughty.
 };
-struct faker *f1=NULL;
+struct faker *f1 = NULL;
 
 static int __init oops2_init(void)
 {
 #if 0
 	f1 = kmalloc(sizeof(struct faker), GFP_KERNEL);
-	if (!f1) {
-	  pr_warn("kmalloc f1 failed");
-          return -ENOMEM;
-        }
-	MSG("sizeof(long) = %d, sizeof(struct faker) = %lu, actual space alloced = %lu\n", 
-	     sizeof(long), sizeof(struct faker), ksize(f1));
+	if (!f1)
+		return -ENOMEM;
+	MSG("sizeof(long) = %d, sizeof(struct faker) = %lu, actual space alloced = %lu\n",
+	    sizeof(long), sizeof(struct faker), ksize(f1));
 #endif
 	MSG("Hello, about to Oops!\n");
 	f1->bad_cache_align = 0xabcd;

2515a30cc4b7d3d516a4ba5b9bd73c1dbcccb0a0
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-03-29 17:12:41 +0530

    test_kasan.sh: better checks

--- kernel_debug/membugs_kasan/test_kasan.sh
+++ kernel_debug/membugs_kasan/test_kasan.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
+# test_kasan.sh
+# Also, IMP to boot system with 'kasan_multi_shot' :
+# Pass kasan_multi_shot (add to kernel cmdline via /boot/cmdline.txt):
+# From https://www.kernel.org/doc/html/latest/admin-guide/kernel-parameters.html
+#  kasan_multi_shot
+#  [KNL] Enforce KASAN (Kernel Address Sanitizer) to print
+#  report on every invalid memory access. Without this
+#  parameter KASAN will print report only for the first
+#  invalid access.
 name=$(basename $0)
 KMOD=membugs_kasan
 
+# this kernel built w/ CONFIG_KASAN on?
+if [ -f /boot/config-$(uname -r) ] ; then
+  grep -q "CONFIG_KASAN is not set" /boot/config-$(uname -r) && {
+    echo "${name}: this kernel (ver $(uname -r)) doesn't seem to have CONFIG_KASAN configured... aborting"
+    exit 1
+  }
+elif [ -f /proc/config.gz ] ; then
+  zcat /proc/config.gz | grep -q "CONFIG_KASAN is not set" && {
+    echo "${name}: this kernel (ver $(uname -r)) doesn't seem to have CONFIG_KASAN configured... aborting"
+    exit 1
+  }
+else
+  echo "${name}: no kernel config to check... assuming CONFIG_KASAN is enabled ?"
+fi
+
 [ ! -f ${KMOD}.ko ] && {
   echo "${name}: kernel module \"${KMOD}.ko\" not present? Aborting ...
-  Build it & retry..."
+  Build it & retry...
+  Also, did you remember to boot the system with the kernel cmdline parameter
+  'kasan_multi_shot'"
   exit 1
 }
 ls -l ${KMOD}.ko
@@ -23,7 +49,9 @@ ls -l ${KMOD}.ko
 	test case  9 : UAR (use-after-return)
 	test case 10 : double-free
 	test case 11 : memory leak : simple leak
-"
+
+  Also, did you remember to boot the system with the kernel cmdline parameter
+  'kasan_multi_shot' "
  exit 1
 }
 

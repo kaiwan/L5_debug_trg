9e97742ec58d5e6de08f738868b021693c4985fa
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-02-10 11:00:03 +0530

    Update em

--- kernel_debug/kprobes_stuff/2_helper_kp/helper_kp.c
+++ kernel_debug/kprobes_stuff/2_helper_kp/helper_kp.c
@@ -15,6 +15,8 @@
  * <kaiwan -at- kaiwantech -dot- com>
  * License: MIT
  */
+#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -23,27 +25,25 @@
 #include <linux/kprobes.h>
 #include "convenient.h"
 
-#define MODULE_VER 		"0.3"
-#define MODULE_NAME    	"helper_kp"
+#define MODULE_VER 		"0.1"
+//#define MODULE_NAME    	"helper_kp"
 
 static char * funcname;
 /* module_param (var, type, sysfs_entry_permissions); 
  *  0 in last => no sysfs entry 
  */
 module_param(funcname, charp, 0);
-MODULE_PARM_DESC(funcname, 
-	"Function name of the target (LKM's) function to attach probe to.");
+MODULE_PARM_DESC(funcname,
+"Function name of the target (LKM's) function to attach probe to.");
 
 static int verbose=0;
-module_param(verbose, int, 0);
+module_param(verbose, int, 0644);
 MODULE_PARM_DESC(verbose, "Set to 1 to get verbose printk's (defaults to 0).");
 
-
 static struct kprobe kpb;
-static struct timeval tv_pre, tv_post, tv_diff;
+static u64 tm_start = 0, tm_end = 0;
 static int running_avg=0;
 static spinlock_t lock;
-
 
 /*
  * This probe runs just prior to "funcptr()" .
@@ -51,11 +51,11 @@ static spinlock_t lock;
 static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
 	spin_lock(&lock);
-	do_gettimeofday(&tv_pre);
+	tm_start = ktime_get_real_ns();
 	spin_unlock(&lock);
 
 	if (verbose) {
-		MSG( "Pre '%s'.\n", funcname);
+		pr_debug( "Pre '%s'.\n", funcname);
 		PRINT_CTX();
 		//dump_stack();
 	}
@@ -67,13 +67,14 @@ static void handler_post(struct kprobe *p, struct pt_regs *regs,
 		unsigned long flags)
 {
 	spin_lock(&lock);
-	do_gettimeofday(&tv_post);
+	tm_end = ktime_get_real_ns();
 
 	if (verbose) {
-		MSG( "%s:%d. Post '%s'.\n",
+		pr_debug( "%s:%d. Post '%s'.\n",
 			current->comm, current->pid, funcname);
 	}
 
+/*
 	timeval_subtract(&tv_diff, &tv_post, &tv_pre);
 	running_avg = (running_avg + tv_diff.tv_usec)/2;
 	if (unlikely(tv_diff.tv_sec > 0)) {
@@ -83,18 +84,19 @@ static void handler_post(struct kprobe *p, struct pt_regs *regs,
 	}
 	if (unlikely(tv_diff.tv_usec > QUITE_LARGE_LATENCY_US))
 		MSG_SHORT(" !!! %s:%d\n", current->comm, current->pid);
+*/
+	SHOW_DELTA(tm_end, tm_start);
 	spin_unlock(&lock);
 }
 
 static int __init helper_kp_init_module(void)
 {
 	if (!funcname) {
-		printk(KERN_INFO "%s: Must pass funcname \
-as a module parameter\n", MODULE_NAME);
+		pr_info("Must pass funcname as a module parameter\n");
 		return -EINVAL;
 	}
 	spin_lock_init(&lock);
-	printk("%s: trapping function %s, verbose mode %s\n", MODULE_NAME, funcname, (verbose==1?"Y":"N"));
+	pr_info("trapping function %s, verbose mode %s\n", funcname, (verbose==1?"Y":"N"));
 
 	/********* Possible SECURITY concern:
  	 * We just assume the pointer passed is valid and okay.
@@ -104,25 +106,24 @@ as a module parameter\n", MODULE_NAME);
 	kpb.post_handler = handler_post;
 	kpb.symbol_name = funcname;
 	if (register_kprobe(&kpb)) {
-		printk(KERN_ALERT "%s: register_kprobe failed!\n"
+		pr_alert("register_kprobe failed!\n"
 		"Check: is function '%s' invalid, static, inline or attribute-marked '__kprobes' ?\n", 
-			MODULE_NAME, funcname);
+			funcname);
 		return -EINVAL;
 	}
-	printk("%s: registered kprobe for function %s\n",  MODULE_NAME, funcname);
+	pr_info("registered kprobe for function %s\n",  funcname);
 	return 0;	/* success */
 }
 
 static void helper_kp_cleanup_module(void)
 {
 	unregister_kprobe(&kpb);
-	printk(KERN_INFO "Unregistered kprobe @ function %s;\n\
- %s %s removed\n", funcname, MODULE_NAME, MODULE_VER);
+	pr_info("Unregistered kprobe @ function %s\n", funcname);
 }
 
 module_init(helper_kp_init_module);
 module_exit(helper_kp_cleanup_module);
 
-MODULE_AUTHOR("Kaiwan N Billimoria <kaiwan@kaiwantech.com>");
+MODULE_AUTHOR("Kaiwan N Billimoria");
 MODULE_DESCRIPTION("Helper Kprobe module; registers a kprobe to the passed function");
 MODULE_LICENSE("Dual MIT/GPL");

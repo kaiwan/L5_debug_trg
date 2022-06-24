99ec66755ed20642f6260786b4b807942765702b
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-06-24 12:14:25 +0530

    Use superior getopts to process all args, allow passing options

--- flamegraph/2flameg.sh
+++ flamegraph/2flameg.sh
@@ -22,9 +22,9 @@
 name=$(basename $0)
 FLMGR=~/FlameGraph  # location of code
 echo "#p = $# p= $*"
-[ $# -lt 3 ] && {
+[ $# -lt 4 ] && {
  echo "${name}: should be invoked by the flame_grapher.sh script, not directly!"
- echo "Usage: ${name} result-folder SVG-filename style-to-display(1 for icicle)"
+ echo "Usage: ${name} result-folder SVG-filename style-to-display(1 for icicle) type(1 for FlameChart)"
  exit 1
 }
 
@@ -37,28 +37,62 @@ cd ${1} || exit 1
 
 INFILE=perf.data
 OUTFILE=${2}
+STYLE=${3}
+TYPE=${4}
+
 [ ! -f ${INFILE} ] && {
   echo "${name} : perf data file ${INFILE} invalid? Aborting..."
   cd ${TOPDIR}
   exit 1
 }
-[ $3 -ne 0 && $3 -ne 1 ] && {
+if [ $3 -ne 0 -a $3 -ne 1 ] ; then
 	echo "${name}: third parameter graph-style must be 0 or 1"
 	exit 1
-}
+fi
 
 echo
 echo "${name}: Working ... generating SVG file \"${1}/${OUTFILE}\"..."
 
-if [ ${3} -eq 0 ] ; then 
-  sudo perf script --input ${INFILE} | ${FLMGR}/stackcollapse-perf.pl | ${FLMGR}/flamegraph.pl > ${OUTFILE} || {
-  echo "${name}: failed."
-  exit 1
-  }
+#---
+# Interesting options to flamegraph.pl:
+# (ref: https://github.com/brendangregg/FlameGraph )
+# --title TEXT     # change title text
+# --subtitle TEXT  # second level title (optional)
+# --inverted       # icicle graph ; downward-growing
+# --flamechart     # produce a flame chart (sort by time, do not merge stacks)
+# --hash           # colors are keyed by function name hash
+# --cp             # use consistent palette (palette.map)
+# --notes TEXT     # add notes comment in SVG (for debugging)
+TITLE="CPU mixed-mode Flame"
+# ${name} result-folder SVG-filename style-to-display(1 for icicle) type(1 for FlameChart)"
+#            p1               p2           p3:STYLE                      p4:TYPE
+[ ${TYPE} -eq 1 ] && PTYPE=--flamechart
+NOTES="notes text: "
+if [ ${STYLE} -eq 0 ] ; then
+   [ ${TYPE} -eq 0 ] && {
+     TITLE="${TITLE}Graph ${OUTFILE} ; style is normal (upward-growing stacks), type is graph"
+	 NOTES="${NOTES}FlameGraph, type normal"
+   } || {
+     TITLE="${TITLE}Graph ${OUTFILE} ; style is normal (upward-growing stacks), type is chart"
+	 NOTES="${NOTES}FlameGraph, type chart"
+   }
+   sudo perf script --input ${INFILE} | ${FLMGR}/stackcollapse-perf.pl | \
+	  ${FLMGR}/flamegraph.pl  --title "${TITLE}" --subtitle "${OUTFILE}" ${PTYPE} > ${OUTFILE} || {
+     echo "${name}: failed."
+     exit 1
+   }
 else
-  sudo perf script --input ${INFILE} | ${FLMGR}/stackcollapse-perf.pl | ${FLMGR}/flamegraph.pl --inverted > ${OUTFILE} || {
-  echo "${name}: failed."
-  exit 1
+  [ ${TYPE} -eq 1 ] && {
+	 TITLE="${TITLE}Chart ${OUTFILE}; style is flamechart (all stacks, X-axis is timeline)"
+	 NOTES="${NOTES}FlameChart, type flamechart"
+  } || {
+	 TITLE="${TITLE}Graph ${OUTFILE}; style is flamegraph (merged stacks)"
+	 NOTES="${NOTES}FlameGraph, type normal"
+  }
+  sudo perf script --input ${INFILE} | ${FLMGR}/stackcollapse-perf.pl | \
+	  ${FLMGR}/flamegraph.pl --title "${TITLE}" --subtitle "${OUTFILE}" --inverted ${PTYPE} --notes "${NOTES}" > ${OUTFILE} || {
+     echo "${name}: failed."
+     exit 1
   }
 fi
 
@@ -68,4 +102,7 @@ cd ${TOPDIR}
 ls -lh ${1}/${OUTFILE}
 echo
 echo "View the above SVG file in your web browser to see and zoom into the CPU FlameGraph."
+
+echo "<NOTES:> in the SVG (if any):"
+grep -w "NOTES\:" ${1}/${OUTFILE}
 exit 0
--- flamegraph/flame_grapher.sh
+++ flamegraph/flame_grapher.sh
@@ -13,20 +13,32 @@
 # License: MIT
 name=$(basename $0)
 FLMGR=~/FlameGraph  # location of code
+PERF_RESULT_DIR_BASE=/tmp/flamegraphs # change to make it non-volatile
 STYLE_INVERTED_ICICLE=1
+TYPE_CHART=0
 
 usage()
 {
-  echo "Usage: ${name} [process-PID-to-sample] svg-out-filename(without .svg)
- No PID implies the entire system is sampled..."
- echo "
-Also note that the default style here is to display the stack frames"
- [ ${STYLE_INVERTED_ICICLE} -eq 0 ] && echo " normally (i.e. reaching/growing 'upward'"
- [ ${STYLE_INVERTED_ICICLE} -eq 1 ] && echo " 'icicle'-style (i.e. downward-growing (technically correct!))"
+  echo "Usage: ${name} -o svg-out-filename(without .svg) [options ...]
+  -o svg-out-filename(without .svg) : name of SVG file to generate (saved under ${PERF_RESULT_DIR_BASE}/)
+Options:
+ [-p PID]     : optional:
+                 PID: generate a FlameGraph for ONLY this process (and it's threads if multithreaded)
+                 If not passed, the *entire system* is sampled...
+ [-s <style>] : optional: 
+                 icicle = draw the stack frames growing downward [default]
+                 normal = draw the stack frames growing upward
+ [-f <type>]  : optional:
+                 graph  = produce a flame graph (X axis is NOT time, merges stacks) [default]
+                   Good for performance outliers (who's eating CPU? using max stack?); works well for multi-threaded apps
+                 chart  = produce a flame chart (sort by time, do not merge stacks)
+                   Good for seeing all calls; works well for single-threaded apps
+ -h|-?        : show this help screen.
 
- echo "
-${name} --help , to show this help screen."
+Note that the FlameGraph SVG (and perf.data file) are stored in the volatile ${PERF_RESULT_DIR_BASE} dir;
+copy them to a non-volatile location to save them."
 }
+
 
 ### "main" here
 
@@ -34,17 +46,17 @@ which perf >/dev/null 2>&1 || {
  echo "${name}: perf not installed? Aborting..."
  exit 1
 }
-[ ! -d ${FLMGR} ] && {
- echo "${name}: FlameGraph not installed?
- You need to install it...
- In your home dir, type:
- git clone https://github.com/brendangregg/FlameGraph"
+[ ! -f ./2flameg.sh ] && {
+ echo "${name}: the part-2 script 2flameg.sh is missing? Aborting..."
  exit 1
 }
-#[ $(id -u) -ne 0 ] && {
-# echo "${name}: must run as root user"
-# exit 1
-#}
+[ ! -d ${FLMGR} ] && {
+ echo "${name}: FlameGraph GitHub repo not installed?
+ You need to (one-time) install it...
+ In your terminal window/shell, type:
+ (cd; git clone https://github.com/brendangregg/FlameGraph)"
+ exit 1
+}
 [ $# -lt 1 ] && {
   usage
   exit 1
@@ -54,37 +66,79 @@ which perf >/dev/null 2>&1 || {
   exit 0
 }
 
+#--- getopts processing
+optspec=":o:p:s:f:h?" # a : after an arg implies it expects an argument
+while getopts "${optspec}" opt
+do
+    #echo "opt=${opt} optarg=${OPTARG}"
+    case "${opt}" in
+			  o)
+	 	        OUTFILE=${OPTARG}
+		        #echo "-o passed; OUTFILE=${OUTFILE}"
+			    ;;
+			  p)
+	 	        PID=${OPTARG}
+		        #echo "-p passed; PID=${PID}"
+				# Check if PID is valid
+			    [ ! -d /proc/${PID} ] && {
+			      echo "${name}: PID ${PID} is an invalid (or dead) process?"
+			      exit 1
+			    }
+			    ;;
+			  s)
+	 	        STYLE=${OPTARG}
+		        #echo "-s passed; STYLE=${STYLE}"
+				if [ "${STYLE}" != "normal" -a  "${STYLE}" != "icicle" ]; then
+					usage ; exit 1
+				fi
+				[ "${STYLE}" = "normal" ] && STYLE_INVERTED_ICICLE=0
+			    ;;
+			  f)
+	 	        TYPE=${OPTARG}
+		        #echo "-f passed; TYPE=${TYPE}"
+				if [ "${TYPE}" != "graph" -a  "${TYPE}" != "chart" ]; then
+					usage ; exit 1
+				fi
+				[ "${TYPE}" = "chart" ] && TYPE_CHART=1
+			    ;;
+			  h|?)
+			    usage
+				exit 0
+				;;
+			  *) echo "Unknown option '-${OPTARG}'" ; usage; exit 1
+				;;
+  	  esac
+done
+shift $((OPTIND-1))
+
+[ -z "${OUTFILE}" ] && {
+		usage ; exit 1
+}
+
 HZ=99
 SVG=""
 PDIR=""
 TOPDIR=${PWD}
-PERF_RESULT_DIR_BASE=/tmp/flamegraphs # change to make it non-volatile
 
 #--- Run the part 2 - generating the FG - on interrupt or exit
-trap 'cd ${TOPDIR}; ./2flameg.sh ${PDIR} ${SVG} ${STYLE_INVERTED_ICICLE}' INT QUIT EXIT
+trap 'cd ${TOPDIR}; ./2flameg.sh ${PDIR} ${SVG} ${STYLE_INVERTED_ICICLE} ${TYPE_CHART}' INT QUIT EXIT
 #---
 
-if [ $# -eq 2 ]; then  #------------------ Profile a particular process
- # Check if PID is valid
- kill -0 $1
- [ $? -ne 0 ] && {
-   echo "${name}: PID $1 is an invalid (or dead) process?"
-   exit 1
- }
- echo "### ${name}: recording samples on process PID ${1} now...
+if [ ${PID} -gt 0 ]; then  #------------------ Profile a particular process
+ echo "### ${name}: recording samples on process PID ${PID} now...
  Press ^C to stop..."
- SVG=${2}.svg
- PDIR=${PERF_RESULT_DIR_BASE}/${2}
+ SVG=${OUTFILE}.svg
+ PDIR=${PERF_RESULT_DIR_BASE}/${OUTFILE}
  #echo "PDIR = ${PDIR} SVG=${SVG}"
  mkdir -p ${PDIR}
  sudo chown -R ${LOGNAME}:${LOGNAME} ${PERF_RESULT_DIR_BASE} 2>/dev/null
  cd ${PDIR}
- sudo perf record -F ${HZ} --call-graph dwarf -p $1 || exit 1  # generates perf.data
+ sudo perf record -F ${HZ} --call-graph dwarf -p ${PID} || exit 1  # generates perf.data
 elif [ $# -eq 1 ]; then  #---------------- Profile system-wide
  echo "### ${name}: recording samples system-wide now...
  Press ^C to stop..."
- SVG=${1}.svg
- PDIR=${PERF_RESULT_DIR_BASE}/${1}
+ SVG=${OUTFILE}.svg
+ PDIR=${PERF_RESULT_DIR_BASE}/${OUTFILE}
  #echo "PDIR = ${PDIR} SVG=${SVG} LOGNAME = $(logname)"
 
  mkdir -p ${PDIR}

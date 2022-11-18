ce6d4687e3c9dab3270b0b4048dd72b7a6c20ebc
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-11-18 09:59:57 +0530

    change timeouts

--- stresstest/stressor.sh
+++ stresstest/stressor.sh
@@ -5,10 +5,15 @@
 
 # Wrapper over stress(1) and stress-ng(1)
 
+[ $# -ne 1 ] && {
+	echo "Usage: $0 duration-in-seconds (for both IO and n/w)"
+	exit 1
+}
+
 #sudo stress -v --cpu 4 --io 4 --vm 4 --hdd 4 &
 # IO, disk
-echo "1. sudo stress -v --io 8 --hdd 8 --timeout 60"
-sudo stress -v --io 8 --hdd 8 --timeout 60
+echo "1. sudo stress -v --io 8 --hdd 8 --timeout $1"
+sudo stress -v --io 8 --hdd 8 --timeout $1
 # net
-echo "2. sudo stress-ng -v --all 0 --class network ? --timeout 60"
-sudo stress-ng -v --all 0 --class network ? --timeout 60
+echo "2. sudo stress-ng -v --all 0 --class network ? --timeout 3"
+sudo stress-ng -v --all 0 --class network ? --timeout 3

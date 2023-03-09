01c960e3322e6585b57c2121e2334420b70b64b6
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-03-09 10:43:01 +0530

    Improve the gdbline script, don't use newlines..

--- kernel_debug/hello_gdb/gdbline.sh
+++ kernel_debug/hello_gdb/gdbline.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #
-# $Id: gdbline,v 1.1 2004/08/02 16:27:55 corbet Exp $
-#
 # gdbline module image
 #
 # Outputs an add-symbol-file line suitable for pasting into gdb to examine
@@ -9,23 +7,27 @@
 # (Here used in conjunction with the hello_gdb.ko kernel module; gdb demo).
 # 
 # [minor changes, kaiwan]
+# Turn on unofficial Bash 'strict mode'! V useful
+# "Convert many kinds of hidden, intermittent, or subtle bugs into immediate, glaringly obvious errors"
+# ref: http://redsymbol.net/articles/unofficial-bash-strict-mode/ 
+set -euo pipefail
+
 name=$(basename $0)
-[ $(id -u) -ne 0 ] && {
+[[ $(id -u) -ne 0 ]] && {
 	echo "${name} requires root."
 	exit 1
 }
-
-if [ $# -ne 2 ]; then
+if [[ $# -ne 2 ]]; then
 	echo "Usage: $0 module-name image-filename"
 	echo "  module-name: name of the (already inserted) kernel module (without the .ko)"
 	echo "  image-filename: pathname to the kernel module."
 	exit 1
 fi
-if [ ! -d /sys/module/$1/sections ]; then
-	echo "$0: $1 not a valid file"
+if [[ ! -d /sys/module/$1/sections ]]; then
+	echo "$0: module not loaded OR $1 not a valid name"
 	exit 1
 fi
-if [ ! -f $2 ]; then
+if [[ ! -f $2 ]]; then
 	echo "$0: $2 not a valid file"
 	exit 1
 fi
@@ -34,17 +36,19 @@ cd /sys/module/$1/sections
 echo "Copy-paste the following lines into GDB"
 echo "---snip---"
 
-[ -f .text ] && {
-   sudo echo -n add-symbol-file $2 `/bin/cat .text`
-   sudo echo  " \\"
-} || [ -f .init.text ] && {
-   sudo echo -n add-symbol-file $2 `/bin/cat .init.text`
+# Don't issue newlines and the continuation \ ; results in GDB err:
+# Unrecognized argument "
+# "
+[[ -f .text ]] && {
+   sudo echo -n "add-symbol-file $2 `/bin/cat .text` "
+   #sudo echo  " \\"
+} || [[ -f .init.text ]] && {
+   sudo echo -n "-s .init.text `/bin/cat .init.text` "
 }
 
 for section in .[a-z]* *; do
-    if [ ${section} != ".text" -o ${section} != ".init.text" ]; then
-	    sudo echo  " \\"
-	    sudo echo -n "       -s" ${section} `/bin/cat ${section}`
+    if [[ ${section} != ".text" || ${section} != ".init.text" ]]; then
+	sudo echo -n " -s" ${section} `/bin/cat ${section}`
     fi
 done
 echo "

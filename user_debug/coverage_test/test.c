ef3facde76997cb832b7f70c0b2f84f07a6c2d33
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2024-04-05 10:23:00 +0530

    initial commit; new 'better' Makefile specifically for code coverage

---
+++ user_debug/coverage_test/test.c
@@ -0,0 +1,14 @@
+int main (void)
+{
+  int i;
+
+  for (i = 1; i < 10000 ; i++)
+    {
+      if (i % 3 == 0)
+        printf ("%d is divisible by 3\n", i);
+      if (i % 11 == 0)
+        printf ("%d is divisible by 11\n", i);
+    }
+
+  return 0;
+}
